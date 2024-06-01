fa6e35d8617438bb62574814114b4ac4350382a2
Kiio__ <caillaud.hugo.44@gmail.com>
2024-06-02 00:05:49 +0200

    fix some json property names

--- app/src/main/java/fr/kiiow/mixapi/models/hench/HenchMix.java
+++ app/src/main/java/fr/kiiow/mixapi/models/hench/HenchMix.java
@@ -1,6 +1,7 @@
 package fr.kiiow.mixapi.models.hench;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import fr.kiiow.mixapi.models.world.Item;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -18,23 +19,28 @@ public class HenchMix {
     @ManyToOne
     @JoinColumn(name = "hench_result_id")
     @JsonIgnoreProperties({"evolutions", "mix"})
+    @JsonProperty(value = "hench_result")
     private Hench henchResult;
 
     @ManyToOne
     @JoinColumn(name = "item_left_id")
+    @JsonProperty(value = "item_left")
     private Item itemLeft;
 
     @ManyToOne
     @JoinColumn(name = "hench_left_id")
     @JsonIgnoreProperties({"evolutions", "mix"})
+    @JsonProperty(value = "hench_left")
     private Hench henchLeft;
 
     @ManyToOne
     @JoinColumn(name = "item_right_id")
+    @JsonProperty(value = "item_right")
     private Item itemRight;
 
     @ManyToOne
     @JoinColumn(name = "hench_right_id")
     @JsonIgnoreProperties({"evolutions", "mix"})
+    @JsonProperty(value = "hench_right")
     private Hench henchRight;
 }
