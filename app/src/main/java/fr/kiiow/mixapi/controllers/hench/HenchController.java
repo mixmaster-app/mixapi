4bf705fa467da5f1d76efc457d991f2121977d17
Kiio__ <caillaud.hugo.44@gmail.com>
2024-06-29 16:06:56 +0200

    Add hench drop rate calculator (#10)

--- app/src/main/java/fr/kiiow/mixapi/controllers/hench/HenchController.java
+++ app/src/main/java/fr/kiiow/mixapi/controllers/hench/HenchController.java
@@ -3,9 +3,11 @@ package fr.kiiow.mixapi.controllers.hench;
 import fr.kiiow.mixapi.controllers.AbstractController;
 import fr.kiiow.mixapi.models.hench.Hench;
 import fr.kiiow.mixapi.models.hench.HenchMix;
+import fr.kiiow.mixapi.services.compute.HenchDropRate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -47,5 +49,21 @@ public class HenchController extends AbstractController implements IHenchControl
             return this._404_Not_Found(null);
         }
         return this.resultOk(hench.get().getMixes());
+    }
+
+    @GetMapping(path = "/hench/{id}/droprate")
+    public ResponseEntity<Double> getDropRate(
+            @PathVariable(name = "id") Integer id,
+            @RequestParam(name = "lvl_user") Integer userLevel,
+            @RequestParam(name = "mark_rate") Integer markRate,
+            @RequestParam(name = "server_rate", required = false, defaultValue = "7") Integer serverRate
+            ) {
+        Optional<Hench> isHench = getDaoManager().getHenchDao().findById(id);
+        if(isHench.isEmpty()) {
+            log.warn("Hench not found with id '{}'", id);
+            return this._404_Not_Found(null);
+        }
+
+        return this.resultOk(HenchDropRate.compute(isHench.get(), userLevel, markRate, serverRate));
     }
 }
